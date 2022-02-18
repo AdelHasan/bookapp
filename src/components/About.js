4bc66111c9510de5556634dd085c737d1f48b154
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-02-17 20:51:08 -0600

    updated About

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ const About = () => {
     <div>
       <h1>About This Project</h1>
       <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id ullamcorper leo, at fringilla libero. Nullam interdum varius odio in faucibus. Sed in velit in risus suscipit pellentesque. Cras sem ligula, laoreet eu luctus vitae, dapibus eget odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ligula sapien, condimentum vitae turpis vel, sollicitudin malesuada risus. Ut felis libero, finibus accumsan sollicitudin ac, finibus quis libero. Morbi feugiat pulvinar sodales. Phasellus eget mauris a risus pretium sagittis.
+      This project was built as my second project during my Software Engineering Immersive cohort at General Assembly. The project was built with React, React Router, and Bootstrap. It uses requests to the Google Books API, and allows users to save links to books to a reading list for future reference. 
+      </p>
+
+      <p>
+        My vision for the use of the App became clearer after building out the core functionality. As it functions now, I can see myself keeping a browser tab open with this app to quickly generate a collection of reading material on a topic I wish to dive deep into. Hence the name "Reasearch Launchpad", I think this app can function as a unique starting point for researching topics. 
       </p>
     </div>
   )
